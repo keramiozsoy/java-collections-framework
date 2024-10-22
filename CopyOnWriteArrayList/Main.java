321daf793603e2b085f2f9a8b0bae728b579e50e
kerami <keramidev@gmail.com>
2024-10-22 21:57:12 +0300

    Update Main.java

--- CopyOnWriteArrayList/Main.java
+++ CopyOnWriteArrayList/Main.java
@@ -2,6 +2,10 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CopyOnWriteArrayListExample {
+    // If the list is being modified by multiple threads (or requires modifications during iteration),
+    // you can use a CopyOnWriteArrayList. This class is designed to handle concurrent modifications
+    // without throwing ConcurrentModificationException because it creates a copy of the list on each modification.
+    
     public static void main(String[] args) {
         List<String> list = new CopyOnWriteArrayList<>();
         list.add("A");
